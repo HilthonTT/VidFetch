2fa4cd67e34a9dfa27812f0a6d9e01c9326451c1
Timothy Tandt <Timothy.tandt@outlook.be>
2023-06-02 20:14:42 +0200

    Initial Commit
    Added the .NET Maui Blazor Hybrid app.
    Clean install.

---
+++ VidFetch/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VidFetch;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ VidFetch/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VidFetch;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
