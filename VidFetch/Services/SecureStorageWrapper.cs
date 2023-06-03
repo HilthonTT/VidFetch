5c294349ed3a7c617e6df7bf3ea4042b1a8d4947
Timothy Tandt <Timothy.tandt@outlook.be>
2023-06-03 16:19:54 +0200

    Implemented base code for Index.razor
    Implemented base HTML and code for testing purposes only.
    Installed YoutubeExplode on the blazor app also to display items.

---
+++ VidFetch/Services/SecureStorageWrapper.cs
@@ -0,0 +1,23 @@
+﻿namespace VidFetch.Services;
+public class SecureStorageWrapper : ISecureStorage
+{
+    public async Task<string> GetAsync(string key)
+    {
+        return await SecureStorage.GetAsync(key);
+    }
+
+    public bool Remove(string key)
+    {
+        return SecureStorage.Remove(key);
+    }
+
+    public void RemoveAll()
+    {
+        SecureStorage.RemoveAll();
+    }
+
+    public async Task SetAsync(string key, string value)
+    {
+        await SecureStorage.SetAsync(key, value);
+    }
+}
