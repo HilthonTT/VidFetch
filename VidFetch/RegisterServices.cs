3324d9ae8a4ae51784dc01f7f8344b5c7898d718
Timothy Tandt <Timothy.tandt@outlook.be>
2023-06-03 14:44:35 +0200

    Dependency injection.
    Added the services to the MauiProgram.cs

---
+++ VidFetch/RegisterServices.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using VidFetch.Data;
+using VidFetchLibrary.Data;
+using VidFetchLibrary.Downloader;
+using VidFetchLibrary.Helpers;
+
+namespace VidFetch;
+public static class RegisterServices
+{
+    public static void ConfigureServices(this MauiAppBuilder builder)
+    {
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddSingleton<IDefaultData, DefaultData>();
+        builder.Services.AddSingleton<IYoutubeDownloader, YoutubeDownloader>();
+        builder.Services.AddSingleton<IDownloadHelper, DownloadHelper>();
+        builder.Services.AddSingleton<IPathHelper, PathHelper>();
+
+        
+        builder.Services.AddSingleton<WeatherForecastService>();
+    }
+}
