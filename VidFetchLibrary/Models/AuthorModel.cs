453e5fc2398815354a1bcf08ae501e712029c2e2
Timothy Tandt <Timothy.tandt@outlook.be>
2023-06-06 20:04:14 +0200

    Implemented Author and Video Models.
    Will be used to store in embedded database.

---
+++ VidFetchLibrary/Models/AuthorModel.cs
@@ -0,0 +1,10 @@
+﻿using SQLite;
+
+namespace VidFetchLibrary.Models;
+public class AuthorModel
+{
+    [PrimaryKey]
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Url { get; set; }
+}
---
+++ VidFetchLibrary/Models/VideoModel.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace VidFetchLibrary.Models;
+public class VideoModel
+{
+    [PrimaryKey]
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string AuthorId { get; set; }
+    public string Url { get; set; }
+    public TimeSpan Duration { get; set; }
+}
