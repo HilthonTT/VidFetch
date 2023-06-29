efe5aa2a51947678e60fb4bd01e0bbc03a05d99e
Timothy Tandt <Timothy.tandt@outlook.be>
2023-06-29 12:57:38 +0200

    Code refactoring
    Moved C# code to separate file.

---
+++ VidFetch/Page-Components/IndexPlaylistVideo.razor.cs
@@ -0,0 +1,223 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using VidFetchLibrary.Models;
+
+namespace VidFetch.Page_Components;
+
+public partial class IndexPlaylistVideo
+{
+    [Parameter]
+    [EditorRequired]
+    public EventCallback<bool> OpenLoading { get; set; }
+
+    private const string FfmpegErrorMessage = "Your ffmpeg path is invalid: Your video resolution might be lower.";
+    private CancellationTokenSource _playlistTokenSource;
+    private CancellationTokenSource _videoTokenSource;
+    private string _playlistUrl = "";
+    private string _searchText = "";
+    private string _currentDownloadingVideo = "";
+    private string _firstVideoInPlaylistUrl = "";
+    private bool _showDialog = false;
+    private double _playlistProgress = 0;
+    private double _videoProgress = 0;
+    
+    private async Task LoadPlaylist()
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(_playlistUrl))
+            {
+                return;
+            }
+
+            if (IsUrlPlaylist())
+            {
+                await LoadPlaylistVideos();
+                _showDialog = true;
+                _firstVideoInPlaylistUrl = _playlistUrl;
+            }
+            else
+            {
+                snackbar.Add("Please enter a playlist url.");
+            }
+
+            _playlistUrl = "";
+        }
+        catch (Exception ex)
+        {
+            snackbar.Add($"Error: {ex.Message}", Severity.Error);
+        }
+    }
+
+    private async Task LoadPlaylistVideos()
+    {
+        await OpenLoading.InvokeAsync(true);
+        var videos = await youtube.GetPlayListVideosAsync(_playlistUrl);
+        foreach (var v in videos)
+        {
+            if (IsVideoNotLoaded(v.VideoId))
+            {
+                videoLibrary.PlaylistVideos.Add(v);
+            }
+        }
+
+        if (settingsLibrary.SaveVideos)
+        {
+            await SavePlaylistVideos();
+        }
+
+        await OpenLoading.InvokeAsync(false);
+    }
+
+    private async Task SavePlaylistVideos()
+    {
+        foreach (var v in videoLibrary.PlaylistVideos)
+        {
+            await videoData.SetVideoAsync(v.Url, v.VideoId);
+        }
+    }
+
+    private async Task DownloadAllPlaylists()
+    {
+        try
+        {
+            if (File.Exists(settingsLibrary.FfmpegPath)is false)
+            {
+                snackbar.Add(FfmpegErrorMessage, Severity.Warning);
+            }
+
+            var cancellationToken = tokenHelper.InitializeToken(ref _playlistTokenSource);
+            var progressReport = new Progress<double>(value =>
+            {
+                UpdateProgress(ref _playlistProgress, value);
+            });
+            foreach (var v in videoLibrary.PlaylistVideos)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                _currentDownloadingVideo = v.Title;
+                await youtube.DownloadVideoAsync(v.Url, progressReport, cancellationToken);
+                AddSnackbar(v.Title);
+            }
+
+            CancelPlaylistDownload();
+        }
+        catch (Exception ex)
+        {
+            snackbar.Add($"Error: {ex.Message}", Severity.Error);
+            await OpenLoading.InvokeAsync(false);
+        }
+    }
+
+    private async Task DownloadVideo(string url)
+    {
+        try
+        {
+            if (File.Exists(settingsLibrary.FfmpegPath)is false)
+            {
+                snackbar.Add(FfmpegErrorMessage, Severity.Warning);
+            }
+
+            var cancellationToken = tokenHelper.InitializeToken(ref _videoTokenSource);
+            var progressReport = new Progress<double>(value =>
+            {
+                UpdateProgress(ref _videoProgress, value);
+            });
+            await youtube.DownloadVideoAsync(url, progressReport, cancellationToken);
+            CancelVideoDownload();
+        }
+        catch (Exception ex)
+        {
+            snackbar.Add($"Error: {ex.Message}", Severity.Error);
+        }
+    }
+
+    private async Task<IEnumerable<string>> SearchPlaylistVideos(string searchInput)
+    {
+        return await searchHelper.SearchAsync(videoLibrary.PlaylistVideos, searchInput);
+    }
+
+    private void FilterPlaylistVideo()
+    {
+        videoLibrary.PlaylistVideos = searchHelper.FilterList(videoLibrary.PlaylistVideos, _searchText);
+    }
+
+    private void UpdateProgress(ref double progressVariable, double value)
+    {
+        progressVariable = value;
+        StateHasChanged();
+    }
+
+    private void CancelPlaylistDownload()
+    {
+        tokenHelper.CancelRequest(ref _playlistTokenSource);
+        _playlistProgress = 0;
+        _currentDownloadingVideo = "";
+    }
+
+    private void CancelVideoDownload()
+    {
+        tokenHelper.CancelRequest(ref _videoTokenSource);
+    }
+
+    private void AddSnackbar(string title)
+    {
+        snackbar.Add($"Successfully downloaded {title}", Severity.Normal);
+    }
+
+    private void ClearPlaylistVideos()
+    {
+        videoLibraryHelper.ClearPlaylistVideos(ref _playlistProgress);
+    }
+
+    private void RemovePlaylistVideo(VideoModel video)
+    {
+        var v = videoLibrary.PlaylistVideos.FirstOrDefault(v => v.VideoId == video.VideoId || v.Url == video.Url);
+        videoLibraryHelper.RemovePlaylistVideo(v);
+    }
+
+    private bool IsUrlPlaylist()
+    {
+        return Uri.IsWellFormedUriString(_playlistUrl, UriKind.Absolute) && _playlistUrl.Contains("list=");
+    }
+
+    private bool IsVideoNotLoaded(string videoId)
+    {
+        return videoLibrary.Videos.Any(v => v.VideoId == videoId)is false;
+    }
+
+    private int GetIndex(VideoModel playlistVideo)
+    {
+        int index = videoLibrary.PlaylistVideos.IndexOf(playlistVideo);
+        return index + 1;
+    }
+
+    private string GetDownloadText()
+    {
+        if (videoLibrary.PlaylistVideos?.Count <= 0)
+        {
+            return "Download Video";
+        }
+
+        if (videoLibrary.PlaylistVideos?.Count == 1)
+        {
+            return "Download 1 Video";
+        }
+
+        return $"Download {videoLibrary.PlaylistVideos?.Count} Videos";
+    }
+
+    private string GetSearchBarText()
+    {
+        if (videoLibrary?.PlaylistVideos.Count <= 0)
+        {
+            return "Search Video";
+        }
+
+        if (videoLibrary.PlaylistVideos?.Count == 1)
+        {
+            return "Search 1 Video";
+        }
+
+        return $"Search {videoLibrary.PlaylistVideos?.Count} Videos";
+    }
+}
---
+++ VidFetch/Pages/Index.razor.cs
@@ -0,0 +1,42 @@
+using MudBlazor;
+
+namespace VidFetch.Pages;
+
+public partial class Index
+{
+    private const string VideoId = "Videos";
+    private const string PlaylistVideoId = "Playlists-Videos";
+    private const string ChannelId = "Channels";
+    private const string PlaylistId = "Playlists";
+    private bool _isVisible = false;
+    private MudTabs _tabs;
+    private MudTabPanel _videoPanel;
+    private MudTabPanel _playlistVideoPanel;
+    private MudTabPanel _channelPanel;
+    private MudTabPanel _playlistPanel;
+    private MudDialog _loadingDialog;
+    private MudProgressCircular _progressCircular;
+
+    private async Task LoadPlaylistVideos(string url)
+    {
+        ToggleLoadingOverlay(true);
+        var videos = await youtube.GetPlayListVideosAsync(url);
+        foreach (var v in videos)
+        {
+            videoLibrary.PlaylistVideos.Add(v);
+        }
+
+        ToggleLoadingOverlay(false);
+        StateHasChanged();
+    }
+
+    private void Activate(object id)
+    {
+        _tabs.ActivatePanel(id);
+    }
+
+    private void ToggleLoadingOverlay(bool show)
+    {
+        _isVisible = show;
+    }
+}
