952d3d83c5e870a6e4afb7aa115acf4c20567f6f
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-07 17:12:03 +0200

    Added the other languages.
    Added French
    Added Indonesian.

--- VidFetchLibrary/Language/LanguageExtension.cs
+++ VidFetchLibrary/Language/LanguageExtension.cs
@@ -17,82 +17,31 @@ public class LanguageExtension : ILanguageExtension
         return settings.SelectedLanguage switch
         {
             Data.Language.English => EnglishDictionary(text),
-            Data.Language.French => new(),
-            Data.Language.Indonesian => new(),
+            Data.Language.French => FrenchDictionary(text),
+            Data.Language.Indonesian => IndonesianDictionary(text),
             _ => EnglishDictionary(),
         };
     }
 
     private static Dictionary<KeyWords, string> EnglishDictionary(string text = "")
     {
-        var englishDictonary = new Dictionary<KeyWords, string>
-        {
-            { KeyWords.PasteALink, "Paste A Link" },
-            { KeyWords.SavedMedias, "Saved Medias" },
-            { KeyWords.SearchMedias , "Search Medias" },
-            { KeyWords.Settings, "Settings" },
-            { KeyWords.Uploaded, "Uploaded" },
-            { KeyWords.Delete , "Delete" },
-            { KeyWords.Save, "Save" },
-            { KeyWords.Videos, "Videos" },
-            { KeyWords.PlaylistWarning, "Warning: Only shows 200 videos for performance." },
-            { KeyWords.LoadMore, "Load More" },
-            { KeyWords.Search, "Search" },
-            { KeyWords.AppSettings, "App Settings" },
-            { KeyWords.DarkMode, "Dark Mode" },
-            { KeyWords.DownloadSubtitles, "Download Subtitles included with the video" },
-            { KeyWords.AutoSaveVideo, "Automatically Save Videos" },
-            { KeyWords.CreateSubdirectory, "Create Sub Directory for playlist" },
-            { KeyWords.RemoveVideoDownload, "Remove Video After Download" },
-            { KeyWords.SelectedPath, "Pick your preferred download path." },
-            { KeyWords.SelectedFormat, "Pick your preferred format." },
-            { KeyWords.SelectedResolution, "Pick your preferred resolution." },
-            { KeyWords.SearchLabelText, $"Search your {text}" },
-            { KeyWords.SearchHelperText, $"Input the URL or the title of the {text}" },
-            { KeyWords.CancelSearch, "Cancel Search" },
-            { KeyWords.OpenFolderLocation, "Open Folder Location" },
-            { KeyWords.Clear, "Clear" },
-            { KeyWords.Video, "Video" },
-            { KeyWords.Channel, "Channel" },
-            { KeyWords.Playlist, "Playlist" },
-            { KeyWords.Download, "Download" },
-            { KeyWords.Cancel, "Cancel" },
-            { KeyWords.UpdateData, "Update Data" },
-            { KeyWords.CancelUpdateData, "Cancel Update Data" },
-            { KeyWords.DeleteAllWarningTitle, "Delete All" },
-            { KeyWords.DeleteAllWarningText, $"Deleting all of your {text} is irreversible!" },
-            { KeyWords.Confirm, "Confirm" },
-            { KeyWords.UrlPlaylistTitle, "It appears your URL is a playlist." },
-            { KeyWords.UrlPlaylistText, "Would you like to download your video's Url?" },
-            { KeyWords.Remove, "Remove" },
-            { KeyWords.OpenDetails, "Open Details" },
-            { KeyWords.OpenUrl, "Open Url" },
-            { KeyWords.Watch, "Watch" },
-            { KeyWords.SuccessfullySettings, "Successfully saved settings." },
-            { KeyWords.FailedSettings, "Failed to save settings." },
-            { KeyWords.FfmpathNotExistError, "Your ffmpeg path doesn't exist." },
-            { KeyWords.FfmpegErrorMessage, "Your ffmpeg path is invalid: Your video resolution might be lower." },
-            { KeyWords.FfmpathCleared, "Cleared your Ffmpeg path." },
-            { KeyWords.SuccessfullySavedData, $"Successfully saved {text}." },
-            { KeyWords.SuccessfullyDeletedData, $"Successfully deleted {text}." },
-            { KeyWords.SuccessfullyDownloaded, $"Successfully downloaded {text}." },
-            { KeyWords.SuccessfullyUpdatedData, $"Successfully updated {text}."},
-            { KeyWords.OperationCancelled, "Operation cancelled" },
-            { KeyWords.ErrorWhileSaving, "An error occured while saving." },
-            { KeyWords.ErrorWhileUpdating, "An error occured while updating." },
-            { KeyWords.ErrorWhileLoadingPlaylist, "An error occured while loading your playlist." },
-            { KeyWords.ErrorWhileLoadingData, "An occured while loading your data." },
-            { KeyWords.NoLongerExistsDelete, $"{text} no longer exists. It has been deleted." },
-            { KeyWords.EnterPlaylistUrl, "Please enter a playlist URL" },
-            { KeyWords.ChannelVideoCount, $"Only showing {text} videos." },
-            { KeyWords.NoVideoErrorMessage, "Error: You have no videos available." },
-            { KeyWords.DownloadingErrorMessage, $"There was an issue downloading your videos." },
-            { KeyWords.English, "English" },
-            { KeyWords.French, "French" },
-            { KeyWords.Indonesian, "Indonesian" },
-        };
+        var englishDictionary = Language.EnglishDictionary.Dictionary(text);
 
-        return englishDictonary;
+        return englishDictionary;
+    }
+
+    private static Dictionary<KeyWords, string> FrenchDictionary(string text = "")
+    {
+        var frenchDictionary = Language.FrenchDictionary.Dictionary(text);
+
+        return frenchDictionary;
+    }
+
+    private static Dictionary<KeyWords, string> IndonesianDictionary(string text = "")
+    {
+        var indonesianDictionary = Language.IndonesianDictionary.Dictionary(text);
+
+        return indonesianDictionary;
     }
 }
 
@@ -144,14 +93,11 @@ public enum KeyWords
     FailedSettings,
     FfmpathNotExistError,
     FfmpathCleared,
-
     SuccessfullyDownloaded,
     SuccessfullySavedData,
     SuccessfullyUpdatedData,
     SuccessfullyDeletedData,
-
     SuccessfullySavedAllVideos,
-
     OperationCancelled,
     ErrorWhileSaving,
     ErrorWhileUpdating,
@@ -159,12 +105,9 @@ public enum KeyWords
     ErrorWhileLoadingData,
     EnterPlaylistUrl,
     NoLongerExistsDelete,
-
-    
     ChannelVideoCount,
     NoVideoErrorMessage,
     DownloadingErrorMessage,
-
     English,
     French,
     Indonesian,
